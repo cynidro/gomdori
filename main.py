482469f14e67a2cfad698171ab41711b4ee4e160
cynidro <kiehlsince@gmail.com>
2026-06-03 11:45:37 +0900

    fix: 네비게이션 및 소요시간 목적지 좌표 오류 수정

--- main.py
+++ main.py
@@ -46,10 +46,10 @@ def get_duration():
 
     url = "https://apis-navi.kakaomobility.com/v1/directions"
     # origin: 동작구 사당로13길 31 (126.9634938,37.4789596)
-    # destination: 서초구 방배로 226 (126.9950796,37.4839884)
+    # destination: 서초구 방배로 226 (126.9880196,37.4839845)
     params = {
         "origin": "126.9634938,37.4789596",
-        "destination": "126.9950796,37.4839884",
+        "destination": "126.9880196,37.4839845",
         "priority": "RECOMMEND"
     }
     headers = {
